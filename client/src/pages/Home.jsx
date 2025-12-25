208798b4aa46f3afeb7b63069b0801d78666aa03
dev27-Kunal <dev27.mxpertz@gmail.com>
2025-12-25 14:21:03 +0530

    Test Submission

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,17 +4,23 @@ import ShopBy from "../components/ShopBy";
 import GenInfo, { Brands } from "../components/GenInfo";
 
 const Home = () => {
+  const data = [
+    { src: "/GenInfo/adidas.jpg", name: "Adidas", to: "/search/adidas" },
+    { src: "/GenInfo/nike.png", name: "Nike", to: "/search/nike" },
+    { src: "/GenInfo/skechers.jpg", name: "Skechers", to: "/search/skechers" },
+    { src: "/GenInfo/puma.jpg", name: "Puma", to: "/search/puma" },
+  ];
   return (
     <div className="max-w-screen-xl xs:w-[95vw] xs:max-w-[95vw] md:w-full mx-auto ">
       <Carousel />
       <GenInfo />
       <Brands />
-      <div className="md:w-full md:max-w-full xs:mx-2  sm:mx-auto ">
+      <div className="">
         <div className="prose prose-2xl">
-          <ShopBy title="Best Sellers" filter="bestSellers" />
+          <ShopBy title="Best Sellers" filter="bestSellers" data={data} />
         </div>
         <div className="prose prose-2xl">
-          <ShopBy title="Top Rated" filter="topRated" />
+          <ShopBy title="Top Rated" filter="topRated" data={data} />
         </div>
       </div>
     </div>
